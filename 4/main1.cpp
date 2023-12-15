12dd9dd17803f73197bef3986dcbac99d2c17476
TPShipilova <138506392+TPShipilova@users.noreply.github.com>
2023-12-16 00:56:37 +0300

    Add files via upload

---
+++ 4/main1.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "GCF/GCF.hpp"
+#include "Sort/sort.hpp"
+
+enum Command {
+    CHANGE_FUNC = 0,
+    GCF = 1,
+    SORT = 2,
+    EXIT = 3
+};
+
+std::istream& operator>>(std::istream& is, Command& cmd){
+    int tmp;
+    while (((is >> tmp) && (tmp < 0 || tmp > 3))){
+        std::cout << "Invalid command" << std::endl;
+    }
+    cmd = static_cast<Command>(tmp);
+    return is;
+}
+
+enum Function {
+    FIRST = 1,
+    SECOND = 2
+}; 
+
+int main(){
+    
+    Command cmd = EXIT;
+    Function func = FIRST;
+
+    std::cout<< "Choose command: \n" << 
+                    "\t0 - switch func in lib,\n" <<  
+                    "\t1 - find greatest common fraction,\n" << 
+                    "\t2 - sort an array,\n" << 
+                    "\t3 - exit." << std::endl;
+    std::cout << "===========================================" << std::endl;
+
+    while (1){
+        if (!(std::cin >> cmd)){
+            std::cout << "Invalid input" << std::endl;
+            break;
+        }
+        switch (cmd)
+        {
+        case CHANGE_FUNC:
+            func = (func == FIRST) ? SECOND : FIRST;
+            std::cout << "Function switched to " << ((func == FIRST) ? "first" : "second") 
+                    << std::endl;
+            break;
+        case GCF:
+            int a, b;
+            std::cin >> a >> b;
+            switch (func)
+            {
+            case FIRST:
+                std::cout << GCF1(a, b) << std::endl;
+                break;
+            case SECOND:
+                std::cout << GCF2(a, b) << std::endl;
+                break;
+            }
+            break;
+        case SORT:
+            int n;
+            std::cin >> n;
+            int * arr;
+            int * sorted;
+            std::cout << "\nEnter an array :"; 
+            for (int i = 0; i < n; ++i)
+                std::cin >> arr[i];
+            switch (func)
+            {
+            case FIRST:
+                sorted = bubble_sort(arr, n);
+                break;
+            case SECOND:
+                sorted = bubble_sort(arr, n);
+                break;
+            }
+            std::cout << std::endl;
+            for (size_t i = 0; i < n; ++i){
+                std::cout << sorted[i] << " ";
+            }
+            std::cout << std::endl;
+            break;
+        case EXIT:
+            exit(0);
+        }
+    }
+    return 0;
+}
---
+++ 4/main2.cpp
@@ -0,0 +1,102 @@
+#include <dlfcn.h>
+#include <iostream>
+
+enum Command {
+    CHANGE_FUNC = 0,
+    GCF = 1,
+    SORT = 2,
+    EXIT = 3
+};
+
+std::istream& operator>>(std::istream& is, Command& cmd){
+    int tmp;
+    while (((is >> tmp) && (tmp < 0 || tmp > 3))){
+        std::cout << "Invalid command" << std::endl;
+    }
+    cmd = static_cast<Command>(tmp);
+    return is;
+}
+
+enum Algo {
+    FIRST = 1,
+    SECOND = 2
+}; 
+
+std::string si_lib_path = "./libGCF.so";
+std::string cd_lib_path = "./libSort.so";
+
+int main(){
+    
+    void* si_lib = dlopen(si_lib_path.c_str(), RTLD_LAZY);
+    void* cd_lib = dlopen(cd_lib_path.c_str(), RTLD_LAZY);
+
+    if (!cd_lib || !si_lib) {
+        std::cout << "Cannot load library: " << dlerror() << '\n';
+        return 1;
+    }
+
+    typedef int (*GCFType)(int, int);
+    typedef int* (*SortType)(int *, int);
+    
+    if (!dlsym(si_lib, "GCF1") || !dlsym(si_lib, "GCF2") || 
+        !dlsym(cd_lib, "bubble_sort") || !dlsym(cd_lib, "hoar_sort")) {
+        std::cout << "Cannot load function: " << dlerror() << '\n';
+        dlclose(si_lib);
+        dlclose(cd_lib);
+        return 1;
+    }
+
+    Command cmd = EXIT;
+    Algo cur_algo = FIRST;
+
+    GCFType GCF_alg = (GCFType)dlsym(si_lib, "GCF1");
+    SortType Sort_alg = (SortType)dlsym(cd_lib, "bubble_sort");
+
+    std::cout<< "Choose command: \n" << 
+                    "\t0 - switch algo in lib,\n" <<  
+                    "\t1 - calculate sin integral,\n" << 
+                    "\t2 - calculate cos derivative,\n" << 
+                    "\t3 - exit." << std::endl;
+    std::cout << "===========================================" << std::endl;
+
+    while (1){
+        if (!(std::cin >> cmd)){
+            std::cout << "Invalid input" << std::endl;
+            break;
+        }
+        switch (cmd)
+        {
+        case CHANGE_FUNC:
+            cur_algo = (cur_algo == FIRST) ? SECOND : FIRST;
+            GCF_alg = (cur_algo == FIRST) ? (GCFType)dlsym(si_lib, "GCF1") : (GCFType)dlsym(si_lib, "GCF2");
+            Sort_alg = (cur_algo == FIRST) ? (SortType)dlsym(cd_lib, "bubble_sort") : (SortType)dlsym(cd_lib, "hoar_sort");
+            std::cout << "Algo switched to " << ((cur_algo == FIRST) ? "first" : "second") << std::endl;
+            break;
+        case GCF:
+            int a, b;
+            std::cin >> a >> b;
+            std::cout << "GCF is: " << GCF_alg(a, b) << std::endl;
+            break;
+        case SORT:
+            int n;
+            std::cin >> n;
+            int * arr;
+            int * sorted;
+            std::cout << "\nEnter an array :"; 
+            for (int i = 0; i < n; ++i)
+                std::cin >> arr[i];
+            sorted = Sort_alg(arr,n);    
+            std::cout << std::endl << "Sorted: ";
+            for (size_t i = 0; i < n; ++i){
+                std::cout << sorted[i] << " ";
+            }
+            std::cout << std::endl;
+            break;
+        case EXIT:
+            exit(0);
+        }
+    }
+    dlclose(si_lib);
+    dlclose(cd_lib);
+    return 0;
+}
